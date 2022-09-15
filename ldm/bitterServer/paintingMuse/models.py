9f48bcc596ed06927b945f02b41b32904cd17ca7
Patrick Ewing <patrick@chanceagency.com>
2022-09-15 10:54:33 -0700

    Getting some initial migrations running, learning as I go

--- ldm/bitterServer/paintingMuse/models.py
+++ ldm/bitterServer/paintingMuse/models.py
@@ -4,22 +4,26 @@ from django.contrib.auth.models import User
 
 import uuid
 
-# class PaintingMutation(models.Model):
-#     prompt = models.CharField(max_length=600)
-#     request_date = models.DateTimeField('date published')
+class PaintingMutation(models.Model):
+    prompt = models.CharField(max_length=600)
+    request_date = models.DateTimeField('date published')
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
+
 class Painting(models.Model):
     prompt = models.TextField
+    name = models.CharField(max_length=280)
+    created_at = models.DateTimeField('date published')
     image_url = models.URLField
-    # mutation = models.ForeignKey(PaintingMutation, on_delete=models.CASCADE)
+    mutation = models.ForeignKey(PaintingMutation, on_delete=models.CASCADE)
     artist_id = models.CharField(max_length=2)
     # player = models.ForeignKey(Player)
     # user = models.ForeignKey(User)
 
     def __str__(self):
         return self.name
+
 # class Player(models.Model):
 #     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
