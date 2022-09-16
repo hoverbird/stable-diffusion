e92755035cca02a63458c331ea3eec964aef2b75
Patrick Ewing <patrick@chanceagency.com>
2022-09-16 14:11:53 -0700

    Delete false start copy of the program

--- paintingMuse/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from paintingMuse.models import Painting
-
-# Register your models here.
-
-admin.site.register(Painting)
--- paintingMuse/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaintingmuseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'paintingMuse'
